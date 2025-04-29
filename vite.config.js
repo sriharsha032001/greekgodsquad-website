ac0944120ad8a2715527f6aa3bd8a5febb5831d4
devops4sure <ramchandrasriharsha@gmail.com>
2025-04-29 16:33:19 +0530

    Added _redirects file and updated vite.config for Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // Set to root if hosted at www.greekgod.life
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: './',
+  server: {
+    open: true,
+  }
 })
